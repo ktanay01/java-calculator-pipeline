2e28fb9ca88e92688de5b0c06441a6ea621538b6
ktanay01 <kumartanay2015@gmail.com>
2025-07-05 23:57:40 +0530

    Added sysout Testing webhook'

--- src/main/java/com/tanay/practice/CalculatorMain.java
+++ src/main/java/com/tanay/practice/CalculatorMain.java
@@ -8,7 +8,7 @@ public class CalculatorMain {
 		int result = c.add(10, 20);
         System.out.println("Result: " + result);
         System.out.println("Enable webhook by using ngrok for automate the build and test process");
-
+        System.out.println("Testing webhook");
 	}
 
 }
